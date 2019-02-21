b0a044c29ef00386bca62d030bca5921249f09f1
John McIver <john.mciver.iii@gmail.com>
2019-02-21 12:12:25 -0700

    Add tuple printing utilities and associated unit tests

---
+++ src/test/TestTupleUtilities.cpp
@@ -0,0 +1,37 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE TestTupleUtilities
+
+#include <tupleUtilities.h>
+
+#include <sstream>
+#include <tuple>
+
+#include <boost/test/unit_test.hpp>
+
+template<class... Args>
+bool operator== ( const std::string& lhs, const std::tuple<Args...>& rhs )
+{
+  std::ostringstream stream;
+  stream << rhs;
+  return lhs.compare( stream.str() ) == 0 ? true : false;
+}
+
+bool operator== ( const std::string& lhs, const std::tuple<>& rhs )
+{
+  std::ostringstream stream;
+  stream << rhs;
+  return lhs.compare(stream.str()) == 0 ? true : false;
+}
+
+BOOST_AUTO_TEST_CASE(testPrintEmpty)
+{
+  const std::tuple<> empty;
+  BOOST_CHECK( "()" == empty );
+}
+
+BOOST_AUTO_TEST_CASE(testPrintOne)
+{
+  const std::tuple<int> testTuple { std::make_tuple( 123 ) };
+  BOOST_CHECK( "(123)" == testTuple );
+}
---
+++ src/tupleUtilities.h
@@ -0,0 +1,52 @@
+#ifndef gsc_boost_safeFloat_tupleUtilities_h
+#define gsc_boost_safeFloat_tupleUtilities_h
+
+#include <ostream>
+#include <tuple>
+
+template<class Tuple, std::size_t N>
+struct TuplePrinter
+{
+  static void print( std::ostream& stream, const Tuple& tuple )
+  {
+    TuplePrinter<Tuple, N-1>::print( tuple );
+    stream << ", " << std::get<N-1>( tuple );
+  }
+};
+
+template<class Tuple>
+struct TuplePrinter<Tuple, 1>
+{
+  static void print( std::ostream& stream, const Tuple& tuple )
+  {
+    stream << std::get<0>( tuple );
+  }
+};
+
+template<class Tuple>
+struct TuplePrinter<Tuple, 0>
+{
+  static void print( std::ostream&, const Tuple& )
+  {
+    // Empty.
+  }
+};
+
+template<class... Args>
+std::ostream& operator<< ( std::ostream& stream,  const std::tuple<Args...>& tuple )
+{
+  stream << "(";
+  TuplePrinter<decltype( tuple ), sizeof...(Args)>::print( stream, tuple );
+  stream << ")";
+  return stream;
+}
+
+std::ostream& operator<< ( std::ostream& stream,  const std::tuple<>& tuple )
+{
+  stream << "(";
+  TuplePrinter<decltype( tuple ), 0>::print( stream, tuple );
+  stream << ")";
+  return stream;
+}
+
+#endif // gsc_boost_safeFloat_tupleUtilities_h
