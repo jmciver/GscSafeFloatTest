aabb59b1519430216bb2fd9b85cafc465d147543
John McIver <john.mciver.iii@gmail.com>
2019-02-24 13:01:23 -0700

    Add tuple of one element implementation of toTupleOfVectors

--- src/test/TestTupleToVector.cpp
+++ src/test/TestTupleToVector.cpp
@@ -3,7 +3,40 @@
 #define BOOST_TEST_MODULE TestTupleToVector
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(testBasic)
+#include <tupleToVector.h>
+#include <tupleUtilities.h>
+
+#include <boost/algorithm/string.hpp>
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+template <class T>
+std::string convertVectorToString( const std::vector< T >& vector )
 {
-  BOOST_TEST(true);
+  std::vector< std::string > vectorAsStrings;
+  std::transform(
+    begin( vector ),
+    end( vector ),
+    std::back_inserter( vectorAsStrings ),
+      []( const T& item ) {
+        std::ostringstream stream;
+        stream << item;
+        return stream.str(); } );
+  return boost::algorithm::join( vectorAsStrings, ", " );
+}
+
+template <class T>
+std::ostream& operator<< ( std::ostream& stream, const std::vector< T >& vector )
+{
+  stream << "(" << convertVectorToString( vector ) << ")";
+  return stream;
+}
+
+BOOST_AUTO_TEST_CASE( testOneElement )
+{
+  BOOST_CHECK( "((1))" == toTupleOfVectors( 1, std::make_tuple( 1 ) ) );
+  BOOST_CHECK( "((2, 2))" == toTupleOfVectors( 2, std::make_tuple( 2 ) ) );
+  BOOST_CHECK( "((3, 3, 3))" == toTupleOfVectors( 3, std::make_tuple( 3 ) ) );
 }
--- src/tupleToVector.h
+++ src/tupleToVector.h
@@ -1,4 +1,25 @@
 #ifndef gsc_boost_safeFloat_tupleToVector_h
 #define gsc_boost_safeFloat_tupleToVector_h
 
+#include <tuple>
+#include <vector>
+
+template<class Tuple, std::size_t size>
+struct TupleElementToVector;
+
+template<class Tuple>
+struct TupleElementToVector<Tuple, 1>
+{
+  static auto build( const std::size_t size, const Tuple& tuple )
+  {
+    return std::make_tuple( std::vector( size, std::get<0>( tuple ) ) );
+  }
+};
+
+template<class... Args>
+auto toTupleOfVectors( const std::size_t size, const std::tuple<Args...>& tuple )
+{
+  return TupleElementToVector<decltype( tuple ), sizeof...(Args)>::build( size, tuple );
+}
+
 #endif // gsc_boost_safeFloat_tupleToVector_h
