f745fbdb396f78e83e99279a65de94c00ca5f88b
John McIver <john.mciver.iii@gmail.com>
2019-02-25 22:15:50 -0700

    Rudimentary getVector(...) implementation with unittests

--- src/test/TestTupleGetVector.cpp
+++ src/test/TestTupleGetVector.cpp
@@ -1,11 +1,19 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
-#define BOOST_TEST_MODULE TestTupleUtilities
+#define BOOST_TEST_MODULE TestTupleGetVector
 #include <boost/test/unit_test.hpp>
 
+#include <tupleToVector.h>
 #include <tupleGetVector.h>
+#include <tupleUtilities.h>
 
-BOOST_AUTO_TEST_CASE( testEmpty )
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE( tupleOfTwoElementVectors )
 {
-  BOOST_CHECK( true );
+  auto testTuple { toTupleOfVectors( 2, std::make_tuple( "one", 2 ) ) };
+  BOOST_CHECK( "{{one, one}, {2, 2}}" == testTuple );
+  auto& element { getVector<1>( testTuple ) };
+  element[1] = 4;
+  BOOST_CHECK( "{{one, one}, {2, 4}}" == testTuple );
 }
--- src/tupleGetVector.h
+++ src/tupleGetVector.h
@@ -1,8 +1,13 @@
 #ifndef gsc_boost_safeFloat_tupleGetVector_h
 #define gsc_boost_safeFloat_tupleGetVector_h
 
+#include <tuple>
+#include <vector>
 
-
-
+template <std::size_t Index, class... Types>
+typename std::tuple_element<Index, std::tuple<Types...> >::type& getVector( std::tuple<Types...>& tuple )
+{
+  return std::get<Index>( tuple );
+}
 
 #endif // gsc_boost_safeFloat_tupleGetVector_h
