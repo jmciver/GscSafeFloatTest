ecaaa633bb058a927fbb73097eb52ccedb9d0eaa
John McIver <john.mciver.iii@gmail.com>
2019-03-02 17:30:52 -0700

    Skeletal version of question 1

---
+++ src/positiveIntegerPowerLiteral.h
@@ -0,0 +1,9 @@
+#ifndef gsc_boost_safeFloat_positiveIntegerPowerLiteral_h
+#define gsc_boost_safeFloat_positiveIntegerPowerLiteral_h
+
+#include <cfenv>
+#include <cmath>
+
+static const float kPositivePowerInteger { 0.5f };
+
+#endif // gsc_boost_safeFloat_positiveIntegerPowerLiteral_h
