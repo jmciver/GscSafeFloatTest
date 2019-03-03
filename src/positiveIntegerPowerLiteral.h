c740a01bf8dc7b1066cbe0131c897d058e56de6a
John McIver <john.mciver.iii@gmail.com>
2019-03-02 18:27:55 -0700

    Runtime sort of working

--- src/positiveIntegerPowerLiteral.h
+++ src/positiveIntegerPowerLiteral.h
@@ -1,24 +1,18 @@
 #ifndef gsc_boost_safeFloat_positiveIntegerPowerLiteral_h
 #define gsc_boost_safeFloat_positiveIntegerPowerLiteral_h
 
+#include <boost/numeric/conversion/cast.hpp>
+
 #include <cfenv>
 #include <cmath>
+#include <iostream>
 
 static const float kPositivePowerInteger { 0.5f };
 
-template <typename T>
-struct PositivePowerIntegerImpl;
-
-template <float Value>
-struct PositivePowerIntegerImpl<Value>
+float operator"" _pipl(long double value)
 {
-  using value = Value;
-};
-
-template <typename Value>
-float operator"" _ppi()
-{
-  retun PositivePowerIntegerImpl<Value>::value;
+  const float valueAsFloat { boost::numeric_cast<float>( value ) };
+  return valueAsFloat;
 }
 
 #endif // gsc_boost_safeFloat_positiveIntegerPowerLiteral_h
