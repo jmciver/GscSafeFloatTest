3fdc1cfda92ff4a07a18a9f83569e7c88dc9391c
John McIver <john.mciver.iii@gmail.com>
2019-03-04 22:19:38 -0700

    Adding supporting constexpr functions for part 1

--- src/positiveIntegerPowerLiteral.h
+++ src/positiveIntegerPowerLiteral.h
@@ -6,12 +6,47 @@
 #include <cfenv>
 #include <cmath>
 #include <iostream>
+#include <limits>
 
-static const float kPositivePowerInteger { 0.5f };
+constexpr const float kPositivePowerInteger { 0.5f };
 
-float operator"" _pipl(long double value)
+constexpr bool inRangeOfFloat( long double value )
 {
-  const float valueAsFloat { boost::numeric_cast<float>( value ) };
+  const auto floatMax { static_cast<long double>( std::numeric_limits<float>::max() ) };
+  const auto floatMin { static_cast<long double>( std::numeric_limits<float>::min() ) };
+  return floatMax >= value && value >= floatMin ? true : false;
+}
+
+constexpr float halfRaisedTo( int exponent )
+{
+  std::feclearexcept( FE_ALL_EXCEPT );
+  return std::pow( kPositivePowerInteger, static_cast<float>( exponent ) );
+}
+
+constexpr bool isDivisableByPowerOfHalf( float dividend )
+{
+  int power { 1 };
+  float divisor { halfRaisedTo( power ) };
+  while ( ! std::fetestexcept( FE_UNDERFLOW ) ) {
+    if ( std::fmod( divisor, dividend ) == 0.0f ) {
+      return true;
+    }
+    divisor = halfRaisedTo( ++power );
+  }
+  return false;
+}
+
+constexpr float operator"" _pipl( long double value )
+{
+  if ( ! inRangeOfFloat( value ) ) {
+    throw std::logic_error( "value cannot be represented as a float" );
+  }
+  std::feclearexcept( FE_ALL_EXCEPT );
+  float valueAsFloat { static_cast<float>( value ) };
+  float integral = 0.5f;
+  if ( std::fmod( valueAsFloat, integral ) != 0.0f ) {
+    throw std::logic_error( "value is not divisable by an integer power of 0.5f" );
+  }
   return valueAsFloat;
 }
 
