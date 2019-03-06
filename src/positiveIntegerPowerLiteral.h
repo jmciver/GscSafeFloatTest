3e874241bf88dd43b6a4ec6b09050cc437f687a6
John McIver <john.mciver.iii@gmail.com>
2019-03-05 22:29:37 -0700

    Refactoring and cleanup of positiveIntegerPowerLiternal functions

--- src/positiveIntegerPowerLiteral.h
+++ src/positiveIntegerPowerLiteral.h
@@ -8,9 +8,7 @@
 #include <iostream>
 #include <limits>
 
-constexpr float kBase { 0.5f };
-
-constexpr float checkedConversionToFloat( long double value )
+constexpr float checkedConversionToFloat( const long double value )
 {
   const auto valueAsFloat { static_cast<float>( value ) };
   return std::isinf( valueAsFloat ) ?
@@ -18,28 +16,30 @@ constexpr float checkedConversionToFloat( long double value )
     valueAsFloat;
 }
 
-constexpr float raiseBaseTo( int exponent )
+constexpr float raiseBaseTo( const float base, const int exponent )
 {
-  return std::pow( kBase, static_cast<float>( exponent ) );
+  return std::pow( base, static_cast<float>( exponent ) );
 }
 
-constexpr bool isIntegerPowerOfBase( const float value )
+constexpr bool isIntegerPowerOfBase( const float value, const float base )
 {
   auto power { 1 };
-  auto powerOf0p5 { raiseBaseTo( power ) };
+  auto powerOf0p5 { raiseBaseTo( base, power ) };
   while ( std::isnormal( powerOf0p5 ) ) {
     if ( powerOf0p5 == value ) {
       return true;
     }
-    powerOf0p5 = raiseBaseTo( ++power );
+    powerOf0p5 = raiseBaseTo( base, ++power );
   }
   return false;
 }
 
-constexpr float operator"" _pip0p5( long double value )
+/// @brief The following literal, "_pip0p5", stands for (p)ositive (i)nteger
+///   (p)ower of 0.5.
+constexpr float operator"" _pip0p5( const long double value )
 {
   const auto valueAsFloat { checkedConversionToFloat( value ) };
-  return isIntegerPowerOfBase( valueAsFloat ) ?
+  return isIntegerPowerOfBase( valueAsFloat, 0.5f ) ?
     valueAsFloat :
     throw std::logic_error( "value is not integer power of 0.5f" );
 }
